4a6420e62976f3e873c96ecca252b47ec5451d6c
lwuotianyi <lwuotianyi@gmail.com>
2026-04-17 21:17:18 -0500

    Just check changes atp.

--- assets/components/fill-blanks/fill_blanks.js
+++ assets/components/fill-blanks/fill_blanks.js
@@ -8,10 +8,10 @@ class FillBlanks {
         this.init();
     }
 
-    init() {
+    async init() {
         this.container.innerHTML = "";
 
-        for(let i = 0; i < this.count; i++){
+        for (let i = 0; i < this.count; i++) {
             const input = document.createElement("input");
             input.type = "text";
             input.maxLength = 1;
@@ -22,24 +22,32 @@ class FillBlanks {
             this.inputs.push(input);
         }
         this.attachEvents();
+
+        const saved = await loadValue(this.container.id);
+        if (saved) {
+            this.inputs.forEach((input, i) => {
+                input.value = saved[i] || "";
+            });
+        }
     }
 
     attachEvents() {
         this.inputs.forEach((input, index) => {
-            input.addEventListener("input", () => {
+            input.addEventListener("input", async () => {
                 if (input.value && index < this.inputs.length - 1) {
                     this.inputs[index + 1].focus();
                 }
-                if(index === this.inputs.length-1){
+                if (index === this.inputs.length - 1) {
                     if (this.matches()) {
                         this.container.dataset.state = "correct";
                     } else {
                         this.container.dataset.state = "incorrect";
                     }
                 }
+                await saveValue(this.container.id, this.getValue());
             });
 
-            input.addEventListener("keydown", (e) => {
+            input.addEventListener("keydown", async (e) => {
                 if (e.key === "Backspace") {
                     this.container.dataset.state = "unfilled";
                     if(!input.value && index > 0) {
@@ -50,6 +58,7 @@ class FillBlanks {
                         prev.setSelectionRange(len, len);
                     }
                 }
+                await saveValue(this.container.id, this.getValue());
             })
         })
     }
@@ -61,6 +70,40 @@ class FillBlanks {
     matches() {
         return this.target ? this.getValue() === this.target : null;
     }
+}
+
+function openDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("Kruskal1", 1);
+
+        request.onupgradeneeded = (e) => {
+            const db = e.target.result;
+            db.createObjectStore("answers");
+        };
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+}
+
+async function saveValue(key, value) {
+    const db = await openDB();
+    const tx = db.transaction("answers", "readwrite");
+    const store = tx.objectStore("answers");
+
+    store.put(value, key);
+
+    return tx.complete;
+}
+
+async function loadValue(key) {
+    const db = await openDB();
+    const tx = db.transaction("answers", "readonly");
+    const store = tx.objectStore("answers");
+
+    return new Promise((resolve) => {
+        const request = store.get(key);
+        request.onsuccess = () => resolve(request.result);
+    });
 }
 
 document.querySelectorAll(".fill-blanks").forEach(el => {
